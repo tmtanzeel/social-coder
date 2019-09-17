232282380937213e77f86457beb4f11b1f93aee9
tmtanzeel <tmtanzeel9@gmail.com>
2019-09-17 13:32:40 +0530

    updated file and font decreased for tags

--- application/src/app/ask/ask.component.ts
+++ application/src/app/ask/ask.component.ts
@@ -38,7 +38,8 @@ export class AskComponent implements OnInit {
       [{ 'size': ['small', false, 'large', 'huge'] }],
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       [{ 'color': [] }, { 'background': [] }],
-      [{ 'font': [] }]
+      [{ 'font': [] }],
+      ['link']
     ]
   }
 
