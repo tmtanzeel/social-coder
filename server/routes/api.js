4abf6491a257130a3623765ec185cb2a85254897
Tanzeel Mirza <tmtanzeel9@gmail.com>
2019-10-26 02:25:31 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,7 +6,7 @@ const Post = require('../models/article');
 const Quest = require('../models/discussion');
 const Article = require('../models/article');
 const Discussion = require('../models/discussion');
-const db = 'mongodb://tanzeel_123:mydbpass@cluster0-shard-00-00-znt38.mongodb.net:27017,cluster0-shard-00-01-znt38.mongodb.net:27017,cluster0-shard-00-02-znt38.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+const db = 'mongodb://<my-username>:<my-db-pass>@cluster0-shard-00-00-znt38.mongodb.net:27017,cluster0-shard-00-01-znt38.mongodb.net:27017,cluster0-shard-00-02-znt38.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 const jwt = require('jsonwebtoken');
 
 mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false }, err => {
