a5cea2317b4c735dde36ec4b9f372ca49029d4c0
tmtanzeel <tmtanzeel9@gmail.com>
2020-01-12 02:42:10 +0530

    added web link tool in contribute editor

--- application/src/app/contribute/contribute.component.ts
+++ application/src/app/contribute/contribute.component.ts
@@ -43,7 +43,8 @@ export class ContributeComponent implements OnInit {
       [{ 'size': ['small', false, 'large', 'huge'] }],
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       [{ 'color': [] }, { 'background': [] }],
-      [{ 'font': [] }]
+      [{ 'font': [] }],
+      ['link']
     ]
   }
 
