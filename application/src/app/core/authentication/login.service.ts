30adb3db910683b8b13be6b11a329093e5437feb
mohitaptence <mohit@aptence.com>
2020-08-30 00:50:48 +0530

    Localstorage using profileservice

---
+++ application/src/app/core/authentication/login.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { UserProfileService } from './user-profile.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(
+    private profileservice: UserProfileService,
+    private router: Router
+  ) { }
+
+  processLogin(user: object) {
+
+    return new Observable(observer => {
+      try {
+        this.extraSteps(user).subscribe({
+          error: err => {
+          },
+          complete: () => {
+            this.router.navigateByUrl('/articles')
+          }
+        })
+      } catch{
+      }
+    });
+  }
+
+  extraSteps(data: any): Observable<any> {
+    return new Observable(observer => {
+      console.log("user's data: ", data);
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('userProfile', JSON.stringify(data['user']));
+      this.profileservice.setUserProfileValue(data['user']);
+      this.profileservice.refreshProfileData(data['user']);
+      observer.complete();
+    });
+
+  }
+
+}
